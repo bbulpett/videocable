1a729debf4cb6f6ae27b87f0653626f03b8cac0a
Barnabas Bulpett <bbulpett@charter.net>
2016-07-07 21:21:30 -0400

    Message model and rooms template

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  root 'rooms#show'
+  
   get 'static/index'
-
-  root 'static#index'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160704161731) do
+ActiveRecord::Schema.define(version: 20160708011257) do
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
